87cc911509ea653ca33d8e445900ec7cd0477344
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-01-25 17:16:30 +0100

    components set

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json());
 const User = require('./controllers/users');
-// app.use(express.static(__dirname + './../angular-client'));
-// app.use(express.static(__dirname + './client/node_modules'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NavComponent,
+    FooterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
