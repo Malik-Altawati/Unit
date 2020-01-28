c5d67472b1e83669b1a52decf79f9b10e570180d
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-01-28 21:19:42 +0100

    good

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     //  connectionString: 'postgres://bifekdyy:ft8HcwOXofltHIqgJjZ4tEzfTiVy5rY1@balarama.db.elephantsql.com:5432/bifekdyy'
     // connectionString: 'postgres://postgres:1234@127.0.0.1:56763//postgres'
-    connectionString : "postgres://postgres:1234@localhost:3000/postgres"
+    connectionString: "postgres://postgres:password@localhost:5432/postgres"
 
 });
 
