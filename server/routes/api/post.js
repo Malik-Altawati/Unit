2b505661084adcea98b4838ccf5efc12339ebf94
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-01-28 20:15:22 +0100

    commit

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,6 +1,54 @@
 const Post = require("./../../../controllers/posts.js");
 
+const IncomingForm = require('formidable').IncomingForm;
+const path = require('path')
+
+const fs = require('fs');
+const uniqueId = require('uuid'); 
+ 
+// app.post('/upload', (req, res) => {
+//     const form = new IncomingForm();
+//     var user_id;
+//     var post;
+//     var link;
+//     form.parse(req, function (err, fields, files) {
+//         user_id = fields.user_id
+//         post = fields.post_text
+
+//         if (err) {
+//             res.send(err)
+//         }
+//         res.end();
+//     });
+
+//     form.on('fileBegin', function (name, file) {
+//         var id = uniqueId()
+//         file.path = 'folders/uploaded/' + id + "." + file.name.split(".")[1];
+//         console.log(path.join(__dirname, "/../Unit/folders/uploaded/", id + "." + file.name.split(".")[1]))
+//         link = path.join(__dirname, "/../Unit/folders/uploaded/", id + "." + file.name.split(".")[1])
+//     });
+
+//     form.on('end', (err, data) => {
+//         console.log("done")
+//         console.log(user_id, post, link)
+//     })
+// });
 function createPost(req, res) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     let postObj = { post : req.body.post, link : req.body.link, user_id : req.body.user_id};
     Post.create(postObj).then(data => {
         if (data) {
