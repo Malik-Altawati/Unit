3c2b48b8eb672e9991dce8af0c48902b56bcd4a8
hashem-buzer <hbuzer98@gmail.com>
2020-01-29 15:18:01 +0100

    edited signup names of the form object-- before pull

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from "./user/login/login.component";
 import { LandingPageComponent } from "./pages/landingPage/landing-page/landing-page.component";
 import { SignupComponent } from "./user/signup/signup.component";
 import { PostComponent } from "./pages/home/post/post.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
   { path: "login", component: LoginComponent },
   { path: "signUp", component: SignupComponent },
-  { path: "post", component: PostComponent }
+  { path: "post", component: PostComponent },
+  { path: "home", component: HomeComponent }
 ];
 
 @NgModule({
--- client/src/app/user/signup/signup.component.ts
+++ client/src/app/user/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-signup",
@@ -12,7 +13,11 @@ export class SignupComponent implements OnInit {
   errors: any = [];
   notifyMessage = "";
 
-  constructor(private fb: FormBuilder, private route: ActivatedRoute) {}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.createForm();
@@ -25,11 +30,10 @@ export class SignupComponent implements OnInit {
 
   createForm() {
     this.signupForm = this.fb.group({
-      name: ["", Validators.required],
-      userName: ["", Validators.required],
+      username: ["", Validators.required],
       email: ["", Validators.required],
       password: ["", Validators.required],
-      cPassword: ["", Validators.required]
+      confirmPassword: ["", Validators.required]
     });
   }
   isInvalidForm(fieldName): boolean {
@@ -44,6 +48,9 @@ export class SignupComponent implements OnInit {
   }
 
   signup() {
+    this.http.post("signup", {}).subscribe(data => {
+      console.log(data);
+    });
     console.log(this.signupForm.value);
   }
 }
