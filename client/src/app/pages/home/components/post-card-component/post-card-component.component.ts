0b67aa11d6a47ce83eb4b8c36d4713dafaf592f0
hashem-buzer <hbuzer98@gmail.com>
2020-02-01 21:40:50 +0100

    post-card-component has been edited

--- client/src/app/pages/home/components/post-card-component/post-card-component.component.ts
+++ client/src/app/pages/home/components/post-card-component/post-card-component.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-post-card-component',
-  templateUrl: './post-card-component.component.html',
-  styleUrls: ['./post-card-component.component.scss']
+  selector: "app-post-card-component",
+  templateUrl: "./post-card-component.component.html",
+  styleUrls: ["./post-card-component.component.scss"]
 })
 export class PostCardComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
