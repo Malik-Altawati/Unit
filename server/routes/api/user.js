26e02dae7601d93d773e521ecc6e50f8d25e3b61
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-02-04 11:06:26 +0100

    update profile view finished

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -189,10 +189,9 @@ function enter(req, res) {
 ////////////////////////////////////////////////////////////////////////////logout request
 function logOut(req, res) {
   console.log(
-    "***********************************************************************************************************************"
+    "*******************************************logged out*****************************"
   );
   var user_id = req.body.id;
-  console.log("idddd", user_id);
   Token.deleteIT(user_id)
     .then(result => {
       res.clearCookie("refreshtoken");
@@ -312,7 +311,7 @@ function UpdateProfilePhoto(req, res) {
   const form = new IncomingForm();
   var user_id;
   var link;
-  form.parse(req, function(err, fields, files) {
+  form.parse(req, function (err, fields, files) {
     user_id = fields.user_id;
     if (err) {
       res.send(err);
@@ -320,7 +319,7 @@ function UpdateProfilePhoto(req, res) {
     res.end();
   });
 
-  form.on("fileBegin", function(name, file) {
+  form.on("fileBegin", function (name, file) {
     var id = uniqueId();
     file.path = "folders/uploaded/" + id + "." + file.name.split(".")[1];
 
